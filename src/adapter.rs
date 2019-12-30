bd184051d01a45bb4ac35161f4c7233e3df56835
Cheng JIANG <cocathecafe@gmail.com>
2019-12-30 13:22:10 +0800

    add .travis.yml

--- src/adapter.rs
+++ src/adapter.rs
@@ -43,7 +43,8 @@ impl<'a> DieselAdapter {
                                 v2 VARCHAR,
                                 v3 VARCHAR,
                                 v4 VARCHAR,
-                                v5 VARCHAR
+                                v5 VARCHAR,
+                                CONSTRAINT unique_key UNIQUE(ptype, v0, v1, v2, v3, v4, v5)
                             );
                         "#,
                     conn_opts.get_table()
@@ -119,136 +120,135 @@ impl Adapter for DieselAdapter {
     fn load_policy(&self, m: &mut Model) -> Result<()> {
         use schema::casbin_rules::dsl::casbin_rules;
 
-        self.pool
+        let conn = self
+            .pool
             .get()
-            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)
-            .and_then(|conn| {
-                casbin_rules
-                    .load::<CasbinRule>(&conn)
-                    .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-            })
-            .and_then(move |rules: Vec<CasbinRule>| {
-                for casbin_rule in &rules {
-                    let rule = self.load_policy_line(casbin_rule);
+            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)?;
 
-                    if let Some(ref ptype) = casbin_rule.ptype {
-                        let sec = ptype;
-                        if let Some(t1) = m.model.get_mut(sec) {
-                            if let Some(t2) = t1.get_mut(ptype) {
-                                if let Some(rule) = rule {
-                                    t2.policy.push(rule);
-                                }
-                            }
+        let rules = casbin_rules
+            .load::<CasbinRule>(&conn)
+            .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)?;
+
+        for casbin_rule in &rules {
+            let rule = self.load_policy_line(casbin_rule);
+
+            if let Some(ref ptype) = casbin_rule.ptype {
+                let sec = ptype;
+                if let Some(t1) = m.model.get_mut(sec) {
+                    if let Some(t2) = t1.get_mut(ptype) {
+                        if let Some(rule) = rule {
+                            t2.policy.push(rule);
                         }
                     }
                 }
+            }
+        }
 
-                Ok(())
-            })
-            .map(|_| ())
+        Ok(())
     }
 
     fn save_policy(&self, m: &mut Model) -> Result<()> {
         use schema::casbin_rules::dsl::casbin_rules;
 
-        self.pool
+        let conn = self
+            .pool
             .get()
-            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)
-            .and_then(|conn| {
-                diesel::delete(casbin_rules)
-                    .execute(&conn)
-                    .map(move |_| conn)
-                    .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-            })
-            .and_then(|conn| {
-                // Todo: correctly return errors
-                if let Some(ast_map) = m.model.get("p") {
-                    for (ptype, ast) in ast_map {
-                        ast.policy.iter().for_each(|rule| {
-                            let new_rule = self.save_policy_line(
-                                ptype,
-                                rule.iter().map(|x| x.as_str()).collect::<Vec<&str>>(),
-                            );
+            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)?;
 
-                            diesel::insert_into(casbin_rules)
-                                .values(&new_rule)
-                                .execute(&conn)
-                                .map_err(|err| Box::new(Error::DieselError(err)));
-                        });
+        diesel::delete(casbin_rules)
+            .execute(&conn)
+            .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)?;
+
+        if let Some(ast_map) = m.model.get("p") {
+            for (ptype, ast) in ast_map {
+                for rule in &ast.policy {
+                    let new_rule = self.save_policy_line(
+                        ptype,
+                        rule.iter().map(|x| x.as_str()).collect::<Vec<&str>>(),
+                    );
+
+                    if let Err(err) = diesel::insert_into(casbin_rules)
+                        .values(&new_rule)
+                        .execute(&conn)
+                        .map_err(|err| Box::new(Error::DieselError(err)))
+                    {
+                        return Err(err);
                     }
                 }
+            }
+        }
 
-                if let Some(ast_map) = m.model.get("g") {
-                    for (ptype, ast) in ast_map {
-                        ast.policy.iter().for_each(|rule| {
-                            let new_rule = self.save_policy_line(
-                                ptype,
-                                rule.iter().map(|x| x.as_str()).collect::<Vec<&str>>(),
-                            );
+        if let Some(ast_map) = m.model.get("g") {
+            for (ptype, ast) in ast_map {
+                for rule in &ast.policy {
+                    let new_rule = self.save_policy_line(
+                        ptype,
+                        rule.iter().map(|x| x.as_str()).collect::<Vec<&str>>(),
+                    );
 
-                            diesel::insert_into(casbin_rules)
-                                .values(&new_rule)
-                                .execute(&conn)
-                                .map_err(|err| Box::new(Error::DieselError(err)));
-                        });
+                    if let Err(err) = diesel::insert_into(casbin_rules)
+                        .values(&new_rule)
+                        .execute(&conn)
+                        .map_err(|err| Box::new(Error::DieselError(err)))
+                    {
+                        return Err(err);
                     }
                 }
+            }
+        }
 
-                Ok(())
-            })
-            .map(|_| ())
+        Ok(())
     }
 
     fn add_policy(&mut self, _sec: &str, ptype: &str, rule: Vec<&str>) -> Result<bool> {
         use schema::casbin_rules::dsl::casbin_rules;
 
-        self.pool
+        let conn = self
+            .pool
             .get()
-            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)
-            .and_then(move |conn| {
-                let new_rule = self.save_policy_line(ptype, rule);
+            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)?;
 
-                diesel::insert_into(casbin_rules)
-                    .values(&new_rule)
-                    .execute(&conn)
-                    .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-            })
-            .map(|_| true)
+        let new_rule = self.save_policy_line(ptype, rule);
+
+        diesel::insert_into(casbin_rules)
+            .values(&new_rule)
+            .execute(&conn)
+            .and_then(|n| if n == 1 { Ok(true) } else { Ok(false) })
+            .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
     }
 
     fn remove_policy(&self, _sec: &str, pt: &str, rule: Vec<&str>) -> Result<bool> {
         use schema::casbin_rules::dsl::*;
 
-        self.pool
+        let conn = self
+            .pool
             .get()
-            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)
-            .and_then(move |conn| {
-                diesel::delete(
-                    casbin_rules.filter(
-                        ptype.eq(pt).and(
-                            v0.is_not_distinct_from(rule.get(0)).and(
-                                v1.is_not_distinct_from(rule.get(1))
-                                    .and(v2.is_not_distinct_from(rule.get(2)))
-                                    .and(
-                                        v3.is_not_distinct_from(rule.get(3))
-                                            .and(v4.is_not_distinct_from(rule.get(4)))
-                                            .and(v5.is_not_distinct_from(rule.get(5))),
-                                    ),
+            .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)?;
+
+        diesel::delete(
+            casbin_rules.filter(
+                ptype.eq(pt).and(
+                    v0.is_not_distinct_from(rule.get(0)).and(
+                        v1.is_not_distinct_from(rule.get(1))
+                            .and(v2.is_not_distinct_from(rule.get(2)))
+                            .and(
+                                v3.is_not_distinct_from(rule.get(3))
+                                    .and(v4.is_not_distinct_from(rule.get(4)))
+                                    .and(v5.is_not_distinct_from(rule.get(5))),
                             ),
-                        ),
                     ),
-                )
-                .execute(&conn)
-                .and_then(|n| {
-                    if n == 1 {
-                        Ok(())
-                    } else {
-                        Err(DieselError::NotFound)
-                    }
-                })
-                .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-            })
-            .map(|_| true)
+                ),
+            ),
+        )
+        .execute(&conn)
+        .and_then(|n| {
+            if n == 1 {
+                Ok(true)
+            } else {
+                Err(DieselError::NotFound)
+            }
+        })
+        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
     }
 
     fn remove_filtered_policy(
@@ -261,108 +261,93 @@ impl Adapter for DieselAdapter {
         use schema::casbin_rules::dsl::*;
 
         if field_index <= 5 && !field_values.is_empty() && field_values.len() <= 6 - field_index {
-            self.pool
+            let conn = self
+                .pool
                 .get()
-                .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)
-                .and_then(|conn| {
-                    if field_index == 0 {
-                        diesel::delete(
-                            casbin_rules.filter(
-                                ptype.eq(pt).and(
-                                    v0.is_not_distinct_from(field_values.get(0)).and(
-                                        v1.is_not_distinct_from(field_values.get(1))
-                                            .and(v2.is_not_distinct_from(field_values.get(2)))
-                                            .and(
-                                                v3.is_not_distinct_from(field_values.get(3))
-                                                    .and(
-                                                        v4.is_not_distinct_from(
-                                                            field_values.get(4),
-                                                        ),
-                                                    )
-                                                    .and(
-                                                        v5.is_not_distinct_from(
-                                                            field_values.get(5),
-                                                        ),
-                                                    ),
-                                            ),
+                .map_err(|err| Box::new(Error::PoolError(err)) as Box<dyn StdError>)?;
+
+            (if field_index == 0 {
+                diesel::delete(
+                    casbin_rules.filter(
+                        ptype.eq(pt).and(
+                            v0.is_not_distinct_from(field_values.get(0)).and(
+                                v1.is_not_distinct_from(field_values.get(1))
+                                    .and(v2.is_not_distinct_from(field_values.get(2)))
+                                    .and(
+                                        v3.is_not_distinct_from(field_values.get(3))
+                                            .and(v4.is_not_distinct_from(field_values.get(4)))
+                                            .and(v5.is_not_distinct_from(field_values.get(5))),
                                     ),
+                            ),
+                        ),
+                    ),
+                )
+                .execute(&conn)
+            } else if field_index == 1 {
+                diesel::delete(
+                    casbin_rules.filter(
+                        ptype.eq(pt).and(
+                            v1.is_not_distinct_from(field_values.get(0))
+                                .and(v2.is_not_distinct_from(field_values.get(1)))
+                                .and(
+                                    v3.is_not_distinct_from(field_values.get(2))
+                                        .and(v4.is_not_distinct_from(field_values.get(3)))
+                                        .and(v5.is_not_distinct_from(field_values.get(4))),
                                 ),
-                            ),
-                        )
-                        .execute(&conn)
-                        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-                    } else if field_index == 1 {
-                        diesel::delete(
-                            casbin_rules.filter(
-                                ptype.eq(pt).and(
-                                    v1.is_not_distinct_from(field_values.get(0))
-                                        .and(v2.is_not_distinct_from(field_values.get(1)))
-                                        .and(
-                                            v3.is_not_distinct_from(field_values.get(2))
-                                                .and(v4.is_not_distinct_from(field_values.get(3)))
-                                                .and(v5.is_not_distinct_from(field_values.get(4))),
-                                        ),
-                                ),
-                            ),
-                        )
-                        .execute(&conn)
-                        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-                    } else if field_index == 2 {
-                        diesel::delete(
-                            casbin_rules.filter(
-                                ptype.eq(pt).and(
-                                    v2.is_not_distinct_from(field_values.get(0))
-                                        .and(v3.is_not_distinct_from(field_values.get(1)))
-                                        .and(v4.is_not_distinct_from(field_values.get(2))),
-                                ),
-                            ),
-                        )
-                        .execute(&conn)
-                        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-                    } else if field_index == 3 {
-                        diesel::delete(
-                            casbin_rules.filter(
-                                ptype.eq(pt).and(
-                                    v3.is_not_distinct_from(field_values.get(0))
-                                        .and(v4.is_not_distinct_from(field_values.get(1)))
-                                        .and(v5.is_not_distinct_from(field_values.get(2))),
-                                ),
-                            ),
-                        )
-                        .execute(&conn)
-                        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-                    } else if field_index == 4 {
-                        diesel::delete(
-                            casbin_rules.filter(
-                                ptype
-                                    .eq(pt)
-                                    .and(v4.is_not_distinct_from(field_values.get(0)))
-                                    .and(v5.is_not_distinct_from(field_values.get(1))),
-                            ),
-                        )
-                        .execute(&conn)
-                        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-                    } else {
-                        diesel::delete(
-                            casbin_rules.filter(
-                                ptype
-                                    .eq(pt)
-                                    .and(v5.is_not_distinct_from(field_values.get(0))),
-                            ),
-                        )
-                        .execute(&conn)
-                        .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
-                    }
-                })
-                .and_then(|n| {
-                    if n == 1 {
-                        Ok(())
-                    } else {
-                        Err(Box::new(Error::DieselError(DieselError::NotFound))
-                            as Box<dyn StdError>)
-                    }
-                })
-                .map(|_| true)
+                        ),
+                    ),
+                )
+                .execute(&conn)
+            } else if field_index == 2 {
+                diesel::delete(
+                    casbin_rules.filter(
+                        ptype.eq(pt).and(
+                            v2.is_not_distinct_from(field_values.get(0))
+                                .and(v3.is_not_distinct_from(field_values.get(1)))
+                                .and(v4.is_not_distinct_from(field_values.get(2))),
+                        ),
+                    ),
+                )
+                .execute(&conn)
+            } else if field_index == 3 {
+                diesel::delete(
+                    casbin_rules.filter(
+                        ptype.eq(pt).and(
+                            v3.is_not_distinct_from(field_values.get(0))
+                                .and(v4.is_not_distinct_from(field_values.get(1)))
+                                .and(v5.is_not_distinct_from(field_values.get(2))),
+                        ),
+                    ),
+                )
+                .execute(&conn)
+            } else if field_index == 4 {
+                diesel::delete(
+                    casbin_rules.filter(
+                        ptype
+                            .eq(pt)
+                            .and(v4.is_not_distinct_from(field_values.get(0)))
+                            .and(v5.is_not_distinct_from(field_values.get(1))),
+                    ),
+                )
+                .execute(&conn)
+            } else {
+                diesel::delete(
+                    casbin_rules.filter(
+                        ptype
+                            .eq(pt)
+                            .and(v5.is_not_distinct_from(field_values.get(0))),
+                    ),
+                )
+                .execute(&conn)
+            })
+            .map_err(|err| Box::new(Error::DieselError(err)) as Box<dyn StdError>)
+            .and_then(|n| {
+                if n == 1 {
+                    Ok(true)
+                } else {
+                    Err(Box::new(Error::DieselError(DieselError::NotFound)) as Box<dyn StdError>)
+                }
+            })
         } else {
             Ok(false)
         }
@@ -375,9 +360,7 @@ mod tests {
 
     #[test]
     fn test_adapter() {
-        use crate::adapter::FileAdapter;
-        use crate::enforcer::Enforcer;
-        use crate::model::Model;
+        use casbin::{Enforcer, FileAdapter, Model};
 
         let mut m = Model::new();
         m.load_model("examples/rbac_model.conf");
